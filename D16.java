9a6b21e40db618a44df404401802ef8ed7e8f79e
sumit065-dot <rajkishore35798@gmail.com>
2026-02-18 22:14:13 +0530

    Success Added

---
+++ D16.java
@@ -0,0 +1,17 @@
+//static block
+class D16
+{
+   static
+    {
+     System.out.println("Static block ");
+     int x=10;
+     int y=60;
+      int p=x*y;
+     System.out.println("P="+p);
+    }
+     public static void main(String a[])
+      {
+     
+      }
+    
+}
