18a3620329df80d492490949185769fb50185e32
sumit065-dot <rajkishore35798@gmail.com>
2026-02-18 22:18:44 +0530

    Success Added

---
+++ D18.java
@@ -0,0 +1,12 @@
+/*Taking Input through Keyboard   
+   1.Through command line
+   2.readLine()
+   3.Scanner class*/
+
+class D18
+{
+    public static void main(String a[])
+    {
+     System.out.println(a[0]);
+    }
+}
