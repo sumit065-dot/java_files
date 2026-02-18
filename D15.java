deb8afd8bb5dcc61c4307819acbdb47764e87a0c
sumit065-dot <rajkishore35798@gmail.com>
2026-02-18 22:12:43 +0530

    Added D15.java

---
+++ D15.java
@@ -0,0 +1,17 @@
+//static block
+class D15
+{
+   static
+    {
+     System.out.println("Static block 1");
+    }
+     public static void main(String a[])
+      {
+      System.out.println("Main Method");
+      }
+    static
+    {
+     System.out.println("Static block 2");
+    }
+
+}
