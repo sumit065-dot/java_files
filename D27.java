6bada90f42673f5a21aa30aea18876cd665fffe4
sumit065-dot <rajkishore35798@gmail.com>
2026-02-26 21:57:44 +0530

    Array in java

---
+++ D27.java
@@ -0,0 +1,12 @@
+/*Array in java:-Contiguous memory allocation
+Data_type arrayname[]=new Data_Type[size]*/
+
+class D27
+{
+   public static void main(String a[])
+    {
+      int arr[]=new int[2];
+      System.out.println(arr[0]);
+       System.out.println(arr[1]);
+    }
+}
