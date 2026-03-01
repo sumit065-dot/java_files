5bc037a1182b12f81eceeb68377186de06b5a948
sumit065-dot <rajkishore35798@gmail.com>
2026-03-01 11:33:16 +0530

    static method

---
+++ D31.java
@@ -0,0 +1,14 @@
+class D31
+{
+   public static void main(String a[])
+    {
+      int arr[]=new int[1];
+      arr[0]=10;
+      disp(arr);
+      System.out.println(arr[0]);
+    }
+    static void disp(int []p)
+    {
+      p[0]=1000;
+    }
+}
