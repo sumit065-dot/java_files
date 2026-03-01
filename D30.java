d24fcf8c7c660c276a3396c26f1b74e0fa6a7ba1
sumit065-dot <rajkishore35798@gmail.com>
2026-03-01 11:25:09 +0530

    Without user input

---
+++ D30.java
@@ -0,0 +1,12 @@
+class D30
+{
+   public static void main(String a[])
+    {
+      int arr[]={88,97,23};
+      
+      System.out.println(arr[0]);
+       System.out.println(arr[1]);
+       System.out.println(arr[2]);
+
+    }
+}
