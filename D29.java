b917032fd5b26f2c49ce0d5fe1785c4c1665d5a2
sumit065-dot <rajkishore35798@gmail.com>
2026-03-01 09:36:27 +0530

    array method

---
+++ D29.java
@@ -0,0 +1,14 @@
+class D29
+{
+   public static void main(String a[])
+    {
+      int arr[]=new int[3];
+      arr[0]=27;
+      arr[1]=30;
+      arr[2]=40;
+      System.out.println(arr[0]);
+       System.out.println(arr[1]);
+       System.out.println(arr[2]);
+
+    }
+}
