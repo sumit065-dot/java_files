e2f77d191d1afcab71f5424f87ab1eec5b0985be
sumit065-dot <rajkishore35798@gmail.com>
2026-03-01 11:39:13 +0530

    null output

---
+++ D32.java
@@ -0,0 +1,14 @@
+//2D Array
+//datatype arrayname[][]=new datatype[size][size];
+
+class D32
+{
+   public static void main(String a[])
+    {
+     int arr[][]=new int[4][];
+     System.out.println(arr[0]);
+     System.out.println(arr[1]);
+     System.out.println(arr[2]);
+     System.out.println(arr[3]);
+    }
+}
